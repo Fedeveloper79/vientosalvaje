8cb58c37de6a3f1f8570760ca05713f1a2c26a5b
Federico Ferro <federicomferro@gmail.com>
2022-10-31 18:42:42 -0300

    primer commit

---
+++ codigo.js
@@ -0,0 +1,125 @@
+class destino {
+    constructor(id, viaje, pago1, pago2, fecha1, fecha2, fecha3) {
+        this.viaje = viaje
+        this.id = id
+        this.pago1 = pago1
+        this.pago2 = pago2
+        this.fecha1 = fecha1
+        this.fecha2 = fecha2
+        this.fecha3 = fecha3
+    }
+}
+
+const Salta = new destino(1, `Salta`, 36000, 52000, `noviembre`, `enero`, `febrero`)
+const Jujuy = new destino(2, `Jujuy`, 42000, 60000, `noviembre`, `enero`, `febrero`)
+const catamarca = new destino(3, `Catamarca`, 48000, 270000, `noviembre`, `enero`, `febrero`)
+
+console.log(Salta, Jujuy, catamarca)
+
+let destinoSeleccionado = parseInt(prompt(`Elegi el destino de tus proximas vacaciones, te proponemos 1.Salta - 2.Jujuy - 3.Catamarca`))
+
+let elijoDestino = false
+
+let infoDestinoSeleccionado
+
+while (elijoDestino === false) {
+
+    if (destinoSeleccionado === Salta.id) {
+        elijoDestino = true
+        infoDestinoSeleccionado = Salta
+    }
+    else if (destinoSeleccionado === Jujuy.id) {
+        elijoDestino = true
+        infoDestinoSeleccionado = Jujuy
+    }
+
+    else if (destinoSeleccionado === catamarca.id) {
+        elijoDestino = true
+        infoDestinoSeleccionado = catamarca
+    }
+
+
+    else {
+        destinoSeleccionado = parseInt(prompt(`Elegi un destino de los habilitados, te proponemos 1.Salta - 2.Jujuy - 3.Catamarca`))
+    }
+
+}
+
+alert(`elegiste ${infoDestinoSeleccionado.viaje}`)
+
+//////////////////////////////////////////////////////
+// forma de pago
+
+
+let formaDePago = parseInt(prompt(`Elegi la forma de pago de tu viaje, 1.Pago contado  - 2.Pago financiado en 6 cuotas `))
+
+
+let elijoFormaDePago = false
+
+let infoFormaDePago
+
+while (elijoFormaDePago === false) {
+
+    if (formaDePago === 1) {
+        elijoFormaDePago = true
+        infoFormaDePago = infoDestinoSeleccionado.pago1
+    }
+    else if (formaDePago === 2) {
+        elijoFormaDePago = true
+        infoFormaDePago = infoDestinoSeleccionado.pago2
+    }
+
+
+
+    else {
+        formaDePago = parseInt(prompt(`Elegi una forma de pago habilitada, te proponemos 1.Precio contado - 2.Precio Financiado`))
+    }
+
+
+}
+
+alert(`elegiste ${infoFormaDePago}`)
+
+// eleccion del mes de viaje
+
+let fechaDeViaje = parseInt(prompt(`Elegi la fecha de tu viaje, 1.Noviembre  - 2.Enero  - 3.Febrero `))
+
+
+let elijoFechaDeViaje = false
+
+let infoFechaDeViaje
+
+while (elijoFechaDeViaje === false) {
+
+    if (fechaDeViaje === 1) {
+        elijoFechaDeViaje = true
+        infoFechaDeViaje = infoDestinoSeleccionado.fecha1
+    }
+    else if (fechaDeViaje === 2) {
+        elijoFechaDeViaje = true
+        infoFechaDeViaje = infoDestinoSeleccionado.fecha2
+    }
+
+    else if (fechaDeViaje === 3) {
+        elijoFechaDeViaje = true
+        infoFechaDeViaje = infoDestinoSeleccionado.fecha3
+    }
+
+
+
+    else {
+        fechaDeViaje = parseInt(prompt(`Elegi una fecha habilitada, te proponemos 1.Noviembre  - 2.Enero  - 3.Febrero `))
+    }
+
+
+}
+
+alert(`elegiste ${infoFechaDeViaje}`)
+
+
+function informacionDeTuViaje (infoDestinoSeleccionado, infoFormaDePago, infoFechaDeViaje) {
+
+    alert (`Listo, tenes armado el viaje de tu vida, nos vamos a ${infoDestinoSeleccionado.viaje}, el costo del mismo es ${infoFormaDePago}, y elegiste el mes ideal, nos vamos en ${infoFechaDeViaje}`)
+}
+
+ informacionDeTuViaje(infoDestinoSeleccionado, infoFormaDePago, infoFechaDeViaje)
